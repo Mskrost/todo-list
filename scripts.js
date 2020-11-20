443e74769c4a1f08a76785044540bb74d65caed7
Mskrost <74321599+Mskrost@users.noreply.github.com>
2020-11-20 22:55:16 +0100

    js updaty

--- scripts.js
+++ scripts.js
@@ -1,8 +1,23 @@
 $('button').click(() => {
     var todo = $('input').val();
-    $('ul').append(`<li>${todo}</li>`);
+    if (todo !== '') {
+        $('ul').append(`<li>${todo}<button class="done">D</button><button class="remove">R</button></li>`);
+        $('input').val('');
+    };
 });
-        
+
+/*
 $('ul').on('click', 'li', (event) => {
     $(event.target).toggleClass('line-through');
+    $(event.target).children('.done').toggleClass('did');
+});
+*/
+
+$('ul').on('click', '.remove', (event) => {
+    $(event.target).parent().remove();
+});
+
+$('ul').on('click', '.done', (event) => {
+    $(event.target).parent().toggleClass('line-through');
+    $(event.target).toggleClass('did');
 });
