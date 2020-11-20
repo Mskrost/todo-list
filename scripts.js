9021b08576d44e4268b5ba4cbb4d9468fc8864d3
Mskrost <74321599+Mskrost@users.noreply.github.com>
2020-11-21 00:35:52 +0100

    updates

--- scripts.js
+++ scripts.js
@@ -1,8 +1,18 @@
 $('button').click(() => {
     var todo = $('input').val();
-    if (todo !== '') {
+    if(todo !== '') {
         $('ul').append(`<li>${todo}<button class="done">D</button><button class="remove">R</button></li>`);
         $('input').val('');
+    };
+});
+
+$('main').on('keypress', (event) => {
+    if(event.which == 13) {
+        var todo = $('input').val();
+        if(todo !== '') {
+            $('ul').append(`<li>${todo}<button class="done">D</button><button class="remove">R</button></li>`);
+            $('input').val('');
+        };
     };
 });
 
